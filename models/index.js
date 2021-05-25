cc15c83b0ebeffa80a732f4b83a73a51e2315214
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-25 13:35:17 -0400

    required the various code bases from the models files into the model index.js file

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const NovelsModel = require('./novels')
+const AuthorsModel = require('./authors')
+const GenresModel = require('./genres')
+const { Sequelize } = require('sequelize')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  Novels,
+  Authors,
+  Genres,
+}
