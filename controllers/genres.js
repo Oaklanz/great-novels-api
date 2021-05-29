c27c29f6c5d5471aa864ae8a69f2767daa73f9f3
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-29 14:13:32 -0400

    wrote the codebase for index.js file

--- controllers/genres.js
+++ controllers/genres.js
@@ -3,9 +3,9 @@ const getAllGenres = async (request, response) => {
   const genres = await models.Genres.findAll({ include: [{ model: models.novelGenres }] })
 
   return response.send(genres) }
-const getGenreById = async (request, response) => { const { id } = request.params
+const getGenresById = async (request, response) => { const { id } = request.params
   const genre = await models.Genres.findOne({ where: { id }, include: [{ model: models.novelGenres }] })
 
   return genre ? response.send(genre) : response.sendStatus(404) }
 
-module.exports = { getAllGenres, getGenreById }
+module.exports = { getAllGenres, getGenresById }
